96331159deca87d50029f8d4b9ec3a86657f780e
wahyusatria1725 <167878444+wahyusatria1725@users.noreply.github.com>
2024-04-25 15:19:34 +0700

    UTS

--- index.php
+++ index.php
@@ -195,6 +195,10 @@ if(isset($_SESSION['login'])){
       <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
       <label for="floatingPassword">Password</label>
     </div>
+    <div class="form-floating">
+      <input name="umur" class="form-control"  class="form-control" placeholder="umur">
+      <label for="floatinginput">Umur</label>  
+    </div>
     <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
     <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
   </form>
